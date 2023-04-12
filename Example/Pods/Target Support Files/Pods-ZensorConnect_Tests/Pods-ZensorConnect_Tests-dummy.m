7595d8019688251aa0951e0e4485feaf94cd9575
Nothing2saY <nothing2say.studio@gmail.com>
2023-04-12 17:12:52 +0900

    Initial commit with CocoaPod

---
+++ Example/Pods/Target Support Files/Pods-ZensorConnect_Tests/Pods-ZensorConnect_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZensorConnect_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZensorConnect_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZensorConnect/ZensorConnect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZensorConnect : NSObject
+@end
+@implementation PodsDummy_ZensorConnect
+@end
